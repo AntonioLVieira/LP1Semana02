464b45f9db6e02e0d762b21731e70ae486b40f68
AntonioLVieira <antonio.allvieira@gmail.com>
2023-03-08 23:45:18 +0000

    Exercise 1

---
+++ ManipulaVar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ManipulaVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int x = int.Parse(args[0]);
+            x++;
+            Console.WriteLine(x);
+            
+            
+        }
+
+    }
+}
+
