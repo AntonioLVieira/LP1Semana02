38972ee6e85c1058fdf141d5308fe047bda6b327
AntonioLVieira <antonio.allvieira@gmail.com>
2023-03-09 00:29:22 +0000

    Exercicio 4, versão Final

--- PerguntaValores/Program.cs
+++ PerguntaValores/Program.cs
@@ -9,18 +9,15 @@ namespace PerguntaValores
             double raio;
             double altura;
             double volume;
+            double superficie;
 
             Console.WriteLine("Qual é a altura do cilindro?");
             raio = double.Parse(Console.ReadLine());
             Console.WriteLine("Qual é o raio do cilindro?");
             altura = double.Parse(Console.ReadLine());
-            volume = Math.PI * raio * raio * altura;
-            
-
-
-
-            
-
+            volume = Math.PI * (raio * raio) * altura;
+            superficie = 2 * Math.PI * (raio + altura);
+            Console.WriteLine("O volume do cilindro é: " + volume + "\nA área de superficie do cilindro é: " + superficie);
         }
     }
 }
