13642329133e9ee01ffd989aa2740ee683dead0b
AntonioLVieira <antonio.allvieira@gmail.com>
2023-03-09 00:08:22 +0000

    Exercício 4

---
+++ PerguntaValores/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PerguntaValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual é a altura do cilindro?");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Qual é o raio do cilindro?");
+            int y = int.Parse(Console.ReadLine());
+            
+
+        }
+    }
+}
