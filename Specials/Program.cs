3b6a1ca83c9e490e374b55366421ce597adf45b7
AntonioLVieira <antonio.allvieira@gmail.com>
2023-03-09 01:49:46 +0000

    Ex.7 - Overflow em tipos reais

--- Specials/Program.cs
+++ Specials/Program.cs
@@ -7,7 +7,11 @@ namespace Specials
         static void Main(string[] args)
         {
             int x = int.MaxValue;
+            double y = double.MaxValue;
+            float z = float.MaxValue;
             Console.WriteLine("Overflow from int.MaxValue is: " + (int) (x + 1));
+            Console.WriteLine("Overflow from double.MaxValue is: " + (double) (y + 1) + " - The value stays the same!");
+            Console.WriteLine("Overflow from float.MaxValue is: " + (float) (z + 1) + " - The value stays the same!");
             
 
             
