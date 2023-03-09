0474c289bf9e0ae80f06291451b97f4687b6040d
AntonioLVieira <antonio.allvieira@gmail.com>
2023-03-09 16:24:43 +0000

    Ex.10 - Triangle, Versão final

---
+++ Triangle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(args[0]);
+            string y = args[1];
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(y);
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
