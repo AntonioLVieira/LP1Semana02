e11a079bd5b0db425656aa8d5834f5d7bf48a466
AntonioLVieira <antonio.allvieira@gmail.com>
2023-03-09 15:53:47 +0000

    Ex.8 - GranpaPatternSwitch last version

--- GranpaPatternSwitch/Program.cs
+++ GranpaPatternSwitch/Program.cs
@@ -5,20 +5,20 @@ namespace ChatGPTGrandfather
     public class Program
     {
         private static void Main(string[] args)
-    
+        {
             // Colocar questão ao utilizador
             Console.Write("Place your question? ");
-            question = Console.ReadLine();
+            string question = Console.ReadLine();
 
             // Verificar se se trata de uma questão conhecida e determinar uma
             // resposta adequada
-             string question, response = question switch
+            string response = question switch
             {
-                1 => "How are you?",
-                2 => "What's your name?",
-                3 => "What's your mission?",
+                "How are you?" => "I'm fine, thank you",
+                "What's your name?" => "Gran'pa",
+                "What's your mission?" => "Destroy mankind!",
                 _ => "You got me, I'm not THAT smart!",
-            }
+            };
 
             // Responder adequadamente
             Console.WriteLine(response);
