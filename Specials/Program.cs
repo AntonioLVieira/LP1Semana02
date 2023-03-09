73c7e33457edf5266da2f7f01fb62bbe651aa1ff
AntonioLVieira <antonio.allvieira@gmail.com>
2023-03-09 01:38:02 +0000

    Ex.7 - Overflow em tipos inteiros

--- Specials/Program.cs
+++ Specials/Program.cs
@@ -6,17 +6,9 @@ namespace Specials
     {
         static void Main(string[] args)
         {
-            double x = double.PositiveInfinity;
-            double y = double.NegativeInfinity;
-            float v = float.PositiveInfinity;
-            float z = float.NegativeInfinity;
-            double i = double.PositiveInfinity / double.PositiveInfinity;
-            float j = float.PositiveInfinity / float.PositiveInfinity;
-
-            Console.WriteLine("Special PositiveInfinity value of double is: " + x + " and the Special NegativeInfinity value of double is : " + y);
-            Console.WriteLine("Special PositiveInfinity value of float is: " + v + " and the Special NegativeInfinity value of float is : " + z);
-            Console.WriteLine("Special Value of double: " + i);
-            Console.WriteLine("Special Value of float: " + j);
+            int x = short.MaxValue;
+            Console.WriteLine("Overflow from short.MaxValue is: " + (short) (x + 1));
+            
 
             
 
