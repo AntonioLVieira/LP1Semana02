20d153567cd3e6ddcfafb1085ba0b546f8468e04
AntonioLVieira <antonio.allvieira@gmail.com>
2024-02-28 21:45:49 +0000

    Exercício 1 - versão final

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insere número inteiro não-negativo: ");
+
+            // Recebe a entrada do usuário e converte para byte
+            if (byte.TryParse(Console.ReadLine(), out byte n))
+            {
+                // Realiza as operações e exibe os resultados
+                Console.WriteLine($"Resultado da operação de decremento de {n}: {--n}");
+                Console.WriteLine($"Resultado da operação de incremento de {n}: {++n}");
+            }
+            else
+            {
+                Console.WriteLine("O número tem de ser positivo e inteiro!.");
+            }
+        }
+    }
+}
