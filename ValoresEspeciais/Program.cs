b149e0474279555cf75f1a6aad45990f1187ee89
AntonioLVieira <antonio.allvieira@gmail.com>
2024-03-01 16:56:09 +0000

    Exercicio 6 max. e min. valores normais

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Byte --> MinValue = {byte.MinValue}, MaxValue = {byte.MaxValue}");
+            Console.WriteLine($"SByte --> MinValue = {sbyte.MinValue}, MaxValue = {sbyte.MaxValue}");
+            Console.WriteLine($"Short --> MinValue = {short.MinValue}, MaxValue = {short.MaxValue}");
+            Console.WriteLine($"UShort --> MinValue = {ushort.MinValue}, MaxValue = {ushort.MaxValue}");
+            Console.WriteLine($"Int --> MinValue = {int.MinValue}, MaxValue = {int.MaxValue}");
+            Console.WriteLine($"UInt --> MinValue = {uint.MinValue}, MaxValue = {uint.MaxValue}");
+            Console.WriteLine($"Long --> MinValue = {long.MinValue}, MaxValue = {long.MaxValue}");
+            Console.WriteLine($"ULong --> MinValue = {ulong.MinValue}, MaxValue = {ulong.MaxValue}");
+            Console.WriteLine($"Float --> MinValue = {float.MinValue}, MaxValue = {float.MaxValue}");
+            Console.WriteLine($"Double --> MinValue = {double.MinValue}, MaxValue = {double.MaxValue}");
+            Console.WriteLine($"Decimal --> MinValue = {decimal.MinValue}, MaxValue = {decimal.MaxValue}");
+            Console.WriteLine($"Char --> MinValue = {char.MinValue}, MaxValue = {char.MaxValue}");
+            Console.WriteLine($"Boolean --> MinValue = {bool.FalseString}, MaxValue = {bool.TrueString}");
+            Console.WriteLine($"DateTime --> MinValue = {DateTime.MinValue}, MaxValue = {DateTime.MaxValue}");
+            Console.WriteLine($"String --> There's no Min or MaxValue");
+            Console.WriteLine($"Object --> There's no Min or MaxValue");
+            Console.ReadLine();
+        }
+    }
+}
