a9c980b86dfd79c95325df0b49be7570b97445bb
AntonioLVieira <antonio.allvieira@gmail.com>
2024-03-01 17:26:18 +0000

    Exercicio 9

---
+++ BreakContinue/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BreakContinue
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            const int maxTurns = 20;
+            int turn = 0;
+            while (true)
+            {
+                turn++;
+
+                if (turn >= maxTurns)
+                {
+                    break;
+                }
+
+                if (turn % 3 == 0 || turn == 10)
+                {
+                    continue;
+                }
+
+                Console.WriteLine($"Turno número {turn}");
+            }
+
+            Console.WriteLine($"O último turno foi {turn}");
+        }
+    }
+}
